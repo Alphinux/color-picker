bfe28fff6cf13d51159979a3870c53e90721e4f5
Tim Ruser <tim@ruser.dev>
2023-04-16 16:41:03 +0200

    second commit

---
+++ src/components/colorPicker.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+<template>
+    <div id="colorPickerHero">
+        <div id="colorPickerMain">
+
+        </div>
+        <div id="hueSlider"></div>
+        <div id="transparencySlider"></div>
+    </div>
+</template>
+<style scoped>
+#colorPickerHero {
+    padding: 15px;
+}
+#colorPickerMain {
+    width: 100%;
+    height: auto;
+    aspect-ratio: 16/9;
+    
+}
+</style>
